8c188f1c0472bc95f046da9b333ae7f48267b1d5
vanorman@ucdavis.edu <vanorman@ucdavis.edu>
2020-06-26 13:22:15 -0700

    Work on layout. Added input options for experiment parameters. Added reactive figures that change based on N.

--- qzenoapplet.py
+++ qzenoapplet.py
@@ -1,7 +1,7 @@
 import sys
 import numpy as np
-from PyQt5.QtWidgets import QSlider, QDoubleSpinBox, QMenu, QLabel, QSizePolicy, QMainWindow, QApplication, QPushButton, QWidget, QAction, QTabWidget, QVBoxLayout, QGroupBox, QHBoxLayout, QGridLayout
-from PyQt5.QtGui import QIcon, QFont, QCursor
+from PyQt5.QtWidgets import QSlider, QLineEdit, QDoubleSpinBox, QMenu, QRadioButton, QLabel, QSizePolicy, QMainWindow, QApplication, QPushButton, QWidget, QAction, QTabWidget, QVBoxLayout, QGroupBox, QHBoxLayout, QGridLayout
+from PyQt5.QtGui import QIcon, QFont, QCursor, QPixmap
 import csv
 import datetime
 from PyQt5.QtCore import pyqtSlot, Qt, QPoint, QDir
@@ -18,15 +18,15 @@ class MainGui(QMainWindow):
         self.font = QFont('Sans Serif', 12)
         app.setFont(self.font)
         self.title = 'Quantum Zeno Effect'
-        self.left = 200
-        self.top = 50
-        self.width = 1400
-        self.height = 625
+        # self.left = 200
+        # self.top = 50
+        # self.width = 1400
+        # self.height = 625
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
+        # self.setGeometry(self.left, self.top, self.width, self.height)
         applet = Applet()
         self.setCentralWidget(applet)
 
@@ -38,110 +38,244 @@ class Applet(QWidget):
         super(Applet, self).__init__()
         self.columns = 10
         self.initapplet()
+        self.calcs = QZenoEffectCalcs()
+        self.sliders = self.sliderN()
+        self.pics = self.figures(self.N)
+        self.exp = self.experiment()
+
+
 
     def initapplet(self):
-        grid = QGridLayout()
 
+        self.grid = QVBoxLayout()
+        # self.type = self.exptype()
 
-        sliders = self.sliderbars()
+        self.grid.addWidget(self.sliders)
+        self.grid.addWidget(self.exp)
+        # self.grid.addWidget(self.type)
+        self.box = QGroupBox()
+        self.box.setLayout(self.grid)
+        self.box.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
 
-        grid.addWidget(sliders)
-        box = QGroupBox()
-        box.setLayout(grid)
-        box.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.layout = QHBoxLayout()
+        self.layout.addWidget(self.box)
+        self.layout.addWidget(self.pics)
 
-        box.setTitle('Instrument Parameters')
-        layout = QHBoxLayout()
-        layout.addWidget(box)
-        self.setLayout(layout)
-
-    def sliderbars(self):
-        groupbox = QGroupBox()
-        layout = QGridLayout()
-        groupbox.setLayout(layout)
-
-        self.sliders = CustomSliders()
-        numsplitterlabel = QLabel('Number of Beamsplitters')
-
-
-        layout.addWidget(self.sliders, 0 , 0)
-
-
-
-        return groupbox
-
-
-    @pyqtSlot()
-    def on_click_def(self):
-        return
-
-class CustomSliders(QWidget):
-    def __init__(self, *args, **kwargs):
-        super(CustomSliders, self).__init__(*args, **kwargs)
-        self.N = 1
-        self.refl = 3.14/self.N
-
-        layout1, self.slider1 = self.sliderN()
-        self.slider1.setValue(self.N)
-
-        layout2, self.slider2 = self.sliderref()
-        self.slider2.setValue(314159/self.N)
-
-        # self.slider1.valueChanged.connect(self.numbersplitterslink())
-
-        vlayout = QVBoxLayout(self)
-        vlayout.addLayout(layout1)
-        vlayout.addLayout(layout2)
-
-        self.slider2.valueChanged.connect(lambda x: self.slider1.setValue(np.pi/x))
-
+        self.setLayout(self.layout)
 
     def sliderN(self):
-        slider = QSlider(Qt.Horizontal)
-        slider.setMinimum(1)
-        slider.setMaximum(10000)
+        self.slider = QSlider(Qt.Horizontal)
+        self.slider.setMinimum(1)
+        self.slider.setMaximum(10000)
+        self.N = self.slider.value()
 
         numbox = QDoubleSpinBox()
-        numbox.setDecimals(2)
-        numbox.setRange(slider.minimum(), slider.maximum())
+        numbox.setDecimals(0)
+        numbox.setRange(self.slider.minimum(), self.slider.maximum())
 
-        slider.valueChanged.connect(numbox.setValue)
-        slider.rangeChanged.connect(numbox.setRange)
-        numbox.valueChanged.connect(slider.setValue)
+        self.slider.valueChanged.connect(numbox.setValue)
+        self.slider.rangeChanged.connect(numbox.setRange)
+        numbox.valueChanged.connect(self.slider.setValue)
+        self.slider.valueChanged.connect(self.On_N_change)
 
-        layout = QHBoxLayout()
-        layout.addWidget(QLabel('Number of Mirrors'))
-        layout.addWidget(numbox)
-        layout.addWidget(slider)
+        layout = QGridLayout()
+        layout.addWidget(QLabel('Number of Beamsplitters'), 0, 0)
+        layout.addWidget(numbox,1 , 0)
+        layout.addWidget(self.slider, 1, 1)
 
-        return layout, slider
+        groupbox = QGroupBox()
+        groupbox.setLayout(layout)
+        groupbox.setTitle("Instrument Parameters")
 
-    def sliderref(self):
-        slider = QSlider(Qt.Horizontal)
-        slider.setMaximum(314159)
+        reflectlbl = QLabel('Reflectivity of Beamsplitters')
 
-        numbox = QDoubleSpinBox()
-        numbox.setDecimals(5)
-        numbox.setRange(slider.minimum(), slider.maximum())
+        self.reflectval = QLabel(str(round(np.pi/self.N, 4)))
+
+        layout.addWidget(reflectlbl, 2, 0)
+        layout.addWidget(self.reflectval, 2, 1)
+        return groupbox
+
+    def figures(self, N):
+
+        groupbox = QGroupBox()
+        groupbox.setStyleSheet('background-color: white;')
+        groupbox.setFixedWidth(1000)
+        layout = QVBoxLayout()
+
+        horizlayout1 = QHBoxLayout()
+        horizlayout1.setContentsMargins(0,0,0,0)
+        horizlayout1.setSpacing(0)
+        groupbox.setLayout(layout)
+        layout.addLayout(horizlayout1)
 
 
-        slider.valueChanged.connect(lambda x: numbox.setValue(x/100000))
-        # slider.rangeChanged.connect(lambda x: numbox.setRange)
-        numbox.valueChanged.connect(lambda x: slider.setValue(x * 100000))
+        fname = 'mirror.png'
+        pix = QPixmap(fname)
+        if N < 6:
+            for i in range(N):
+                beamsplitter_i = QLabel()
+                beamsplitter_i.setPixmap(pix)
+                horizlayout1.addWidget(beamsplitter_i)
 
-        layout = QHBoxLayout()
-        layout.addWidget(QLabel('Reflectivity of Mirrors'))
-        layout.addWidget(numbox)
-        layout.addWidget(slider)
+        else:
+            for i in range(6):
+                beamsplitter_i = QLabel()
+                beamsplitter_i.setPixmap(pix)
+                horizlayout1.addWidget(beamsplitter_i)
 
-        return layout, slider
+        det = QLabel()
+        # detpic = QPixmap('detector.png')
+        det.setPixmap(QPixmap('detector.png'))
+        horizlayout1.addWidget(det)
+
+        horizlayout2 = QHBoxLayout()
+        horizlayout2.setSpacing(0)
+        horizlayout2.setContentsMargins(0,0,0,0)
+        layout.addLayout(horizlayout2)
+        if N < 6:
+            circuit = self.calcs.zenocirc(N, 1)
+
+            circuit.draw(filename='circuit.png', output='mpl')
+            cir = QLabel()
+            cirpic = QPixmap('circuit.png')
+            cirpic.scaledToWidth(500)
+            cirpic.scaledToHeight(500)
+
+            cir.setPixmap(cirpic)
+
+
+            horizlayout2.addWidget(cir)
+        else:
+            circuit = self.calcs.zenocirc(6, 1)
+
+            circuit.draw(filename='circuit.png', output='mpl')
+            cir = QLabel()
+            cirpic = QPixmap('circuit.png')
+            cirpic.scaledToWidth(500)
+            cirpic.scaledToHeight(500)
+
+            cir.setPixmap(cirpic)
+
+
+            horizlayout2.addWidget(cir)
+        return groupbox
+
+    def experiment(self):
+        nphotonslbl = QLabel('Number of Photons Sent through System:')
+        self.nphotons = QLineEdit()
+        self.nphotons.setText('1000')
+
+        layout = QGridLayout()
+        box = QGroupBox()
+        box.setLayout(layout)
+        box.setTitle('Experiment Parameters')
+
+        layout.addWidget(nphotonslbl, 0, 0)
+        layout.addWidget(self.nphotons,1,0)
+
+        exptypelayout = QHBoxLayout()
+        self.typebeam = QRadioButton('Beamsplitters')
+        self.typebeam.setChecked(True)
+        self.typebeam.toggled.connect(lambda : self.exptypechange(self.typebeam))
+        exptypelayout.addWidget(self.typebeam)
+
+        self.typepolar = QRadioButton('Polarizers')
+        typebox = QGroupBox()
+        self.typepolar.toggled.connect(lambda : self.exptypechange(self.typepolar))
+        exptypelayout.addWidget(self.typepolar)
+        typebox.setLayout(exptypelayout)
+        typebox.setTitle('Experiment Type')
+        layout.addWidget(typebox,3,0,2,1)
+
+        objecttypelayout = QHBoxLayout()
+        self.yesobject = QRadioButton('Present')
+        self.yesobject.setChecked(True)
+        self.yesobject.toggled.connect(lambda: self.objtypechange(self.yesobject))
+        objecttypelayout.addWidget(self.yesobject)
+
+        self.noobject = QRadioButton('Not Present')
+        self.noobject.toggled.connect(lambda: self.objtypechange(self.noobject))
+        objecttypelayout.addWidget(self.noobject)
+        objbox = QGroupBox()
+        objbox.setLayout(objecttypelayout)
+        objbox.setTitle('Is an Object Present')
+
+        layout.addWidget(objbox, 6, 0, 2, 1)
+
+        return box
+
+    # def objectexist(self):
+    #
+
 
     @pyqtSlot()
+    def On_N_change(self):
+        self.N = self.slider.value()
+        self.layout.removeWidget(self.pics)
+        self.pics = self.figures(self.N)
+        self.layout.addWidget(self.pics)
+        self.reflectivity = str(round(np.pi/self.N, 4))
+        self.reflectval.setText(self.reflectivity)
 
-    def numbersplitterslink(self):
-        N = self.slider1.value()
-        self.slider2.setValue(np.pi/N)
-        return
+    def exptypechange(self, type):
+        if type.text() == 'Beamsplitters':
+            if type.isChecked() ==True:
+                self.exptype = 1
+        if type.text() == 'Polarizers':
+            if type.isChecked() ==True:
+                self.exptype = 0
+
+    def objtypechange(self, type):
+        if type.text() == 'Present':
+            if type.isChecked() ==True:
+                self.objtype = 1
+        if type.text() == 'Not Present':
+            if type.isChecked() ==True:
+                self.objtype = 0
+
+
+class QZenoEffectCalcs():
+    def theta(self, N):
+        theta = np.pi/N
+        return theta
+
+    def transmitpredict(self, obj, N):
+        theta = self.theta(N)
+        if obj == 1:
+            predict = (np.cos(theta/2)**2)**N
+        if obj == 0:
+            predict = 1-(np.sin(theta/2)**2)**N
+        return predict
+
+    def zenocirc(self, N, obj):
+        theta = self.theta(N)
+
+        q = QuantumRegister(1)
+        c = ClassicalRegister(N)
+        circuit = QuantumCircuit(q,c)
+        for i in range(N):
+            circuit.rx(theta, q[0])
+            circuit.measure(q[0], c[i])
+
+        return circuit
+
+    def measure(self, nphotons, N, obj):
+        circuit = self.zenocirc(N)
+        endstate = '0' * N
+
+        result = execute(circuit, backend=self.backend, shots=nphotons).result()
+        counts = result.get_counts()
+        ntrans = counts.get(endstate)
+        pct = ntrans/nphotons
+        predict = self.transmitpredict(obj, N)
+        fname = 'circuit.png_'+N
+        circuit.draw(filename=fname, output='mpl')
+
+        return result, counts, ntrans, pct, predict
+
+
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
